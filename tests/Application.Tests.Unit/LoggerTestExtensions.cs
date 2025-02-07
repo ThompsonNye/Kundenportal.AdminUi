73733331ab112676ec986744ebcdad8744e103a5
ThompsonNye <88248872+ThompsonNye@users.noreply.github.com>
2025-02-07 18:10:30 +0100

    Fix and add tests

---
+++ tests/Application.Tests.Unit/LoggerTestExtensions.cs
@@ -0,0 +1,38 @@
+﻿using System.Text.RegularExpressions;
+using FluentAssertions;
+using Microsoft.Extensions.Logging;
+using NSubstitute;
+using NSubstitute.Core;
+
+namespace Application.Tests.Unit;
+
+public static partial class LoggerTestExtensions
+{
+    public static void ReceivedLog<T>(this ILogger<T> logger, LogLevel logLevel, string message,
+        params object?[] parameters)
+    {
+        MatchCollection matches = LoggingParametersRegex().Matches(message);
+
+        if (matches.Count != parameters.Length)
+        {
+            throw new InvalidOperationException(
+                "Found different number of log message parameters than object parameters");
+        }
+
+        for (var i = 0; i < matches.Count; i++)
+        {
+            var match = matches[i];
+            message = message.Replace(match.Value, parameters[i]?.ToString());
+        }
+        
+        logger.Received().Log(
+            logLevel,
+            Arg.Any<EventId>(),
+            Arg.Is<object>(x => x.ToString() == message),
+            null,
+            Arg.Any<Func<object, Exception?, string>>());
+    }
+
+    [GeneratedRegex(@"\{([\w\d]+)\}")]
+    private static partial Regex LoggingParametersRegex();
+}
--- tests/Application.Tests.Unit/StructureGroupsServiceTests.cs
+++ tests/Application.Tests.Unit/StructureGroupsServiceTests.cs
@@ -1,14 +1,12 @@
 using AutoFixture;
 using FluentAssertions;
-using Kundenportal.AdminUi.Application;
 using Kundenportal.AdminUi.Application.Abstractions;
 using Kundenportal.AdminUi.Application.Models;
 using Kundenportal.AdminUi.Application.Options;
 using Kundenportal.AdminUi.Application.Services;
 using Kundenportal.AdminUi.Application.StructureGroups;
-using Kundenportal.AdminUi.Infrastructure.Persistence;
-using MassTransit.Configuration;
-using Microsoft.EntityFrameworkCore;
+using MassTransit;
+using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 using NSubstitute;
 using NSubstitute.ExceptionExtensions;
@@ -21,16 +19,23 @@ public class StructureGroupsServiceTests
     private readonly IApplicationDbContext _dbContext = InMemoryDbContextProvider.GetDbContext();
     private readonly INextcloudApi _nextcloudApi = Substitute.For<INextcloudApi>();
     private readonly IOptions<NextcloudOptions> _nextcloudOptions = Substitute.For<IOptions<NextcloudOptions>>();
+    private readonly IPublishEndpoint _publishEndpoint = Substitute.For<IPublishEndpoint>();
+    private readonly ILogger<StructureGroupsService> _logger = Substitute.For<ILogger<StructureGroupsService>>();
 
     private readonly Fixture _fixture = new();
-    
+
     public StructureGroupsServiceTests()
     {
-        _sut = new StructureGroupsService(_dbContext, _nextcloudApi, _nextcloudOptions);
+        _sut = new StructureGroupsService(
+            _dbContext,
+            _nextcloudApi,
+            _nextcloudOptions,
+            _publishEndpoint,
+            _logger);
     }
-    
+
     #region GetAllAsync
-    
+
     [Fact]
     public async Task GetAllAsync_ShouldReturnEmptyArray_WhenNoStructureGroupsExist()
     {
@@ -42,7 +47,7 @@ public class StructureGroupsServiceTests
         // Assert
         result.Should().BeEmpty();
     }
-    
+
     [Fact]
     public async Task GetAllAsync_ShouldReturnItems_WhenItemsExist()
     {
@@ -58,11 +63,11 @@ public class StructureGroupsServiceTests
         // Assert
         result.Should().BeEquivalentTo(structureGroups);
     }
-    
+
     #endregion
 
     #region GetPendingAsync
-    
+
     [Fact]
     public async Task GetPendingAsync_ShouldReturnEmptyArray_WhenNoStructureGroupsExist()
     {
@@ -74,7 +79,7 @@ public class StructureGroupsServiceTests
         // Assert
         result.Should().BeEmpty();
     }
-    
+
     [Fact]
     public async Task GetPendingAsync_ShouldReturnItems_WhenItemsExist()
     {
@@ -90,7 +95,44 @@ public class StructureGroupsServiceTests
         // Assert
         result.Should().BeEquivalentTo(structureGroups);
     }
-    
+
+    #endregion
+
+    #region AddPendingAsync
+
+    [Fact]
+    public async Task AddPendingAsync_ShouldAddPendingStructureGroup_WhenCalled()
+    {
+        // Arrange
+        PendingStructureGroup pendingStructureGroup = _fixture.Create<PendingStructureGroup>();
+
+        // Act
+        await _sut.AddPendingAsync(pendingStructureGroup);
+
+        // Assert
+        _dbContext.PendingStructureGroups
+            .Should()
+            .ContainSingle(x => x.Id == pendingStructureGroup.Id);
+    }
+
+    [Fact]
+    public async Task AddPendingAsync_ShouldLogMessages_WhenCalled()
+    {
+        // Arrange
+        PendingStructureGroup pendingStructureGroup = _fixture.Create<PendingStructureGroup>();
+
+        // Act
+        await _sut.AddPendingAsync(pendingStructureGroup);
+
+        // Assert
+        _logger.ReceivedLog(
+            LogLevel.Debug,
+            "Creating pending structure group with id {Id} and name {Name}",
+            pendingStructureGroup.Id,
+            pendingStructureGroup.Name);
+        _logger.Received().LogInformation("Pending structure group created");
+    }
+
     #endregion
 
     #region DoesStructureGroupExistAsync
@@ -114,14 +156,15 @@ public class StructureGroupsServiceTests
     }
 
     [Fact]
-    public async Task DoesStructureGroupExistAsync_ShouldReturnTrue_WhenNextcloudReturnsFolderDetailsForStructureInRootFolder()
+    public async Task
+        DoesStructureGroupExistAsync_ShouldReturnTrue_WhenNextcloudReturnsFolderDetailsForStructureInRootFolder()
     {
         // Arrange
         const string path = "path";
 
         NextcloudOptions nextcloudOptions = new NextcloudOptions();
         _nextcloudOptions.Value.Returns(nextcloudOptions);
-        
+
         NextcloudFolder nextcloudFolder = _fixture.Create<NextcloudFolder>();
         _nextcloudApi.GetFolderDetailsAsync($"{nextcloudOptions.StructureBasePath}/{path}").Returns(nextcloudFolder);
 
@@ -133,7 +176,8 @@ public class StructureGroupsServiceTests
     }
 
     [Fact]
-    public async Task DoesStructureGroupExistAsync_ShouldReturnTrue_WhenNextcloudReturnsFolderDetailsForStructureInSubFolder()
+    public async Task
+        DoesStructureGroupExistAsync_ShouldReturnTrue_WhenNextcloudReturnsFolderDetailsForStructureInSubFolder()
     {
         // Arrange
         const string path = "path";
@@ -143,7 +187,7 @@ public class StructureGroupsServiceTests
             StructureBasePath = "/Structures"
         };
         _nextcloudOptions.Value.Returns(nextcloudOptions);
-        
+
         NextcloudFolder nextcloudFolder = _fixture.Create<NextcloudFolder>();
         _nextcloudApi.GetFolderDetailsAsync($"{nextcloudOptions.StructureBasePath}/{path}").Returns(nextcloudFolder);
 
