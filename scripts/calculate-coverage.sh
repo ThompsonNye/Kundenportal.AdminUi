89bcf8223b6bd31206ee24abf53c366f0cc3ff07
ThompsonNye <88248872+ThompsonNye@users.noreply.github.com>
2025-02-07 18:10:19 +0100

    Calculate coverage directly in job

--- scripts/calculate-coverage.sh
+++
@@ -1,3 +0,0 @@
-# $1 --> The path to the coverage report
-# $2 --> What coverage to calculate (e.g. line or branch)
-grep -A 1 -i "<th>$2 coverage:<\/th>" $1 | grep -o -E '[[:digit:]]+ of [[:digit:]]+' | awk '{printf "Coverage: %.2f%\n", ($1 / $3)*100}'
