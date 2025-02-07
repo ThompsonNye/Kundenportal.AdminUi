9129c23381a0d6a63c031403ee557afbef9c0f54
ThompsonNye <88248872+ThompsonNye@users.noreply.github.com>
2025-02-07 18:10:27 +0100

    Move messaging registration to infrastructure as it is an infrastructure concern

--- src/Application/DependencyInjectionExtensions.cs
+++ src/Application/DependencyInjectionExtensions.cs
@@ -1,8 +1,4 @@
-﻿using FluentValidation;
-using Kundenportal.AdminUi.Application.Filters;
-using Kundenportal.AdminUi.Application.Preferences;
-using Kundenportal.AdminUi.Application.StructureGroups;
-using MassTransit;
+﻿using Kundenportal.AdminUi.Application.StructureGroups;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace Kundenportal.AdminUi.Application;
@@ -12,45 +8,6 @@ public static class DependencyInjectionExtensions
     public static IServiceCollection AddApplicationServices(this IServiceCollection services)
     {
         services.AddScoped<IStructureGroupsService, StructureGroupsService>();
-
-        services.AddMessaging();
-        
-        return services;
-    }
-
-    private static IServiceCollection AddMessaging(this IServiceCollection services)
-    {
-        services.AddMassTransit(x =>
-        {
-            x.SetKebabCaseEndpointNameFormatter();
-            
-            x.AddConsumers(typeof(IApplicationMarker).Assembly);
-            x.AddRequestClient<GetUserPreferences>();
-            
-            x.UsingRabbitMq((context, cfg) =>
-            {
-                cfg.UseConsumeFilter(typeof(ValidationFilter<>), context);
-
-                cfg.UseDelayedRedelivery(r =>
-                {
-                    r.Intervals(
-                        TimeSpan.FromMinutes(5),
-                        TimeSpan.FromMinutes(15),
-                        TimeSpan.FromMinutes(30));
-                    r.Ignore<ValidationException>();
-                });
-                cfg.UseMessageRetry(r =>
-                {
-                    r.Incremental(
-                        5,
-                        TimeSpan.Zero,
-                        TimeSpan.FromSeconds(5));
-                    r.Ignore<ValidationException>();
-                });
-                
-                cfg.ConfigureEndpoints(context);
-            });
-        });
 
         return services;
     }
--- src/Infrastructure/DependencyInjectionExtensions.cs
+++ src/Infrastructure/DependencyInjectionExtensions.cs
@@ -1,5 +1,10 @@
-﻿using Kundenportal.AdminUi.Application.Abstractions;
+﻿using FluentValidation;
+using Kundenportal.AdminUi.Application;
+using Kundenportal.AdminUi.Application.Abstractions;
+using Kundenportal.AdminUi.Application.Filters;
+using Kundenportal.AdminUi.Application.Preferences;
 using Kundenportal.AdminUi.Infrastructure.Persistence;
+using MassTransit;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -12,6 +17,8 @@ public static class DependencyInjectionExtensions
         IConfiguration configuration)
     {
         services.AddApplicationDbContext(configuration);
+
+        services.AddMessaging();
 
         return services;
     }
@@ -31,6 +38,43 @@ public static class DependencyInjectionExtensions
         });
 
         services.AddScoped<IApplicationDbContext>(sp => sp.GetRequiredService<ApplicationDbContext>());
+
+        return services;
+    }
+
+    private static IServiceCollection AddMessaging(this IServiceCollection services)
+    {
+        services.AddMassTransit(x =>
+        {
+            x.SetKebabCaseEndpointNameFormatter();
+
+            x.AddConsumers(typeof(IApplicationMarker).Assembly);
+            x.AddRequestClient<GetUserPreferences>();
+
+            x.UsingRabbitMq((context, cfg) =>
+            {
+                cfg.UseConsumeFilter(typeof(ValidationFilter<>), context);
+
+                cfg.UseDelayedRedelivery(r =>
+                {
+                    r.Intervals(
+                        TimeSpan.FromMinutes(5),
+                        TimeSpan.FromMinutes(15),
+                        TimeSpan.FromMinutes(30));
+                    r.Ignore<ValidationException>();
+                });
+                cfg.UseMessageRetry(r =>
+                {
+                    r.Incremental(
+                        5,
+                        TimeSpan.Zero,
+                        TimeSpan.FromSeconds(5));
+                    r.Ignore<ValidationException>();
+                });
+
+                cfg.ConfigureEndpoints(context);
+            });
+        });
 
         return services;
     }
