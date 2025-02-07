82ca4dfde12c6144401969402463ace264ebfd25
ThompsonNye <88248872+ThompsonNye@users.noreply.github.com>
2025-02-07 18:10:22 +0100

    Move model

--- src/Application/Models/ApplicationUser.cs
+++ src/Application/Models/ApplicationUser.cs
@@ -1,6 +1,6 @@
 using Microsoft.AspNetCore.Identity;
 
-namespace Kundenportal.AdminUi.Application;
+namespace Kundenportal.AdminUi.Application.Models;
 
 // Add profile data for application users by adding properties to the ApplicationUser class
 public class ApplicationUser : IdentityUser
