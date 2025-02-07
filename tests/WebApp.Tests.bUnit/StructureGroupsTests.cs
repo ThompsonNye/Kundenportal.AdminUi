a20e0a22a778c324450298e9d6c114a7cb889152
ThompsonNye <88248872+ThompsonNye@users.noreply.github.com>
2025-02-07 18:10:20 +0100

    Add bUnit test project

---
+++ tests/WebApp.Tests.bUnit/StructureGroupsTests.cs
@@ -0,0 +1,29 @@
+﻿using Kundenportal.AdminUi.WebApp.Components.Pages.Structures;
+using Microsoft.Extensions.Logging;
+using NSubstitute;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebApp.Tests.bUnit;
+
+public class StructureGroupsTests : TestContext
+{
+    private readonly ILogger<StructureGroups> _logger = Substitute.For<ILogger<StructureGroups>>();
+
+    [Fact]
+    public void Option1LogsOptions()
+    {
+        Services.AddSingleton(_logger);
+
+        var cut = RenderComponent<StructureGroups>();
+
+        //cut.Find("#optionen1").Click();
+
+        cut.Find("#option-1").Click();
+
+        _logger.Received().LogInformation("Option 1 ausgewählt");
+    }
+}
